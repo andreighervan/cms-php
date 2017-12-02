e0b5fc7e2520e6d5ff7263d732500b52133d7eb4
andreighervan <andrei.ghervan7@yahoo.com>
2017-12-02 12:42:12 +0200

    Update files v2

--- index.php
+++ index.php
@@ -1,68 +1,12 @@
 <?php
-include 'includes/header.php';
-include 'includes/navigation.php';
-?>
-<!-- Page Content -->
-    <div class="container">
 
-        <div class="row">
+/** @var Silex\Application|false $app */
+$app = require __DIR__ . '/app/web.php';
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+// If we're running PHP's built-in webserver, `web.php` returns `false`,
+// meaning the path is a file. If so, we pass it along.
+if ($app === false) {
+    return false;
+}
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-
-                <!-- First Blog Post -->
-                <?php
-                $query="SELECT * FROM posts";
-                $select_all_posts_query=mysqli_query($conn,$query);
-                while($row=mysqli_fetch_assoc($select_all_posts_query)){
-                    $post_title=$row['post_title'];
-                    $post_author=$row['post_author'];
-                    $post_date=$row['post_date'];
-                    $post_image=$row['post_image'];
-                    $post_content=$row['post_content'];
-                }
-                ?>
-                <h2>
-                    <a href="#"><?php echo $post_title;?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                
-                <hr>
-
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
-
-            </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-            <?php
-            include 'includes/sidebar.php';
-            ?>
-
-        </div>
-        <!-- /.row -->
-
-        <hr>
-<?php
-include 'includes/footer.php';
-?>
+$app->run();
